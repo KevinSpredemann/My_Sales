83301e9d1de9e1fcbbffda3bdcb8f89f177bfffc
Kevin Ruan Spredemann <kevinspredemann50@gmail.com>
2025-02-13 22:11:35 -0300

    feat: service show product

--- src/modules/products/database/repositories/ProductsRepositories.ts
+++ src/modules/products/database/repositories/ProductsRepositories.ts
@@ -5,4 +5,7 @@ export const productRepositories = AppDataSource.getRepository(Product).extend({
   async findByName(name: string): Promise<Product | null> {
     return this.findOneBy({ name });
   },
+  async findById(id: string): Promise<Product | null> {
+    return this.findOneBy({ id });
+  }
 });
