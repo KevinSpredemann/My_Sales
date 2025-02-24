547d53e3a1c346bc17b75523762db329f4319f1a
Kevin Ruan Spredemann <kevinspredemann50@gmail.com>
2025-02-24 19:29:59 -0300

    feat: entity, repository and service userToken

---
+++ src/modules/users/database/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,19 @@
+import AppError from '@shared/errors/AppError';
+import { usersRepositories } from '../repositories/UsersRepositories';
+import { userTokensRepositories } from '../repositories/UserTokensRepositories';
+
+interface IForgotPassword {
+  email: string;
+}
+export default class SendForgotPasswordEmailService {
+  async execute({ email }: IForgotPassword): Promise<void> {
+    const user = await usersRepositories.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('User not found', 404);
+    }
+
+    const token = await userTokensRepositories.generate(user.id);
+    console.log(token);
+  }
+}
